219725375f855906c4592831b1005618086045db
Shubham Chaudhary <sc3617378@gmail.com>
2025-12-08 10:53:12 +0530

    added GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { siteConfig } from "@/src/lib/constants";
-import Navbar from "@/src/components/layout/Navbar";
-import Footer from "@/src/components/layout/Footer";
+import Script from "next/script";
+import LayoutWrapper from "@/src/components/layout/LayoutWrapper";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,22 +24,38 @@ export const metadata: Metadata = {
     "SEO services",
     "social media marketing",
   ],
-  icons:{
-   icon: "/icons/favicon.jpg"
-  }
+  icons: {
+    icon: "/icons/favicon.jpg",
+  },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N5BM99CP');
+        `}
+      </Script>
+
       <body className={`${inter.className} bg-white text-gray-800 antialiased`}>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N5BM99CP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {/* LayoutWrapper handles Navbar/Footer conditionally */}
+        <LayoutWrapper>
+          {children}
+        </LayoutWrapper>
       </body>
     </html>
   );
